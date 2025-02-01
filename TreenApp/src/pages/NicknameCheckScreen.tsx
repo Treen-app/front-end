0e82d65e78b43acb1d3202bd0b5da9304b334096
joiejuni <lumiere.298@gmail.com>
2025-02-01 23:03:32 +0900

    [ADD] 회원가입 화면 비밀번호 유효성 검사 추가

--- TreenApp/src/pages/NicknameCheckScreen.tsx
+++ TreenApp/src/pages/NicknameCheckScreen.tsx
@@ -3,9 +3,12 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-nativ
 
 const NicknameCheckScreen = () => {
   const [userId, setUserId] = useState('');
-  const [isDuplicate, setIsDuplicate] = useState(null); // null: 초기 상태, true: 중복, false: 사용 가능
+  const [isDuplicate, setIsDuplicate] = useState<boolean | null>(null); // 중복 확인 상태
+  const [isIdChecked, setIsIdChecked] = useState(false); // 아이디 확인 완료 여부
   const [password, setPassword] = useState('');
-  const [isIdChecked, setIsIdChecked] = useState(false); // 아이디 확인이 완료되었는지 상태 관리
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [isPasswordValid, setIsPasswordValid] = useState(false); // 비밀번호 유효성 검사 여부
+  const [isPasswordConfirmed, setIsPasswordConfirmed] = useState(false); // 비밀번호 확인 여부
 
   const checkDuplicate = async () => {
     try {
@@ -26,17 +29,39 @@ const NicknameCheckScreen = () => {
     }
   };
 
+  // 비밀번호 유효성 검사 함수
+  const validatePassword = (password: string) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/;
+    setIsPasswordValid(passwordRegex.test(password)); // 비밀번호가 유효한지 확인
+  };
+
+  // 비밀번호 확인
+  const handlePasswordConfirm = (confirmPassword: string) => {
+    setPasswordConfirm(confirmPassword);
+    // 비밀번호 확인 값과 최초 비밀번호 값이 일치하는지 비교
+    setIsPasswordConfirmed(password === confirmPassword); 
+  };
+
+  // 제목을 동적으로 설정하는 부분
+  const getTitle = () => {
+    if (!isIdChecked) {
+      return "아이디를 입력해주세요";
+    }
+    else {
+      return "비밀번호를 입력해주세요";
+    }
+  };
+  
+
   return (
     <View style={styles.container}>
-      <Text style={styles.subtitle}>정보 입력</Text>
-      <Text style={styles.title}>
-        닉네임을 입력해주세요
-      </Text>
+      <Text style={styles.subtitle}>02 정보 입력</Text>
+      <Text style={styles.title}>{getTitle()}</Text>
 
       <View style={styles.inputRow}>
         <TextInput
             style={[styles.input, { flex: 1 }]}
-            placeholder="아이디 입력"
+            placeholder="아이디를 입력해주세요"
             value={userId}
             onChangeText={setUserId}
         />
@@ -61,9 +86,52 @@ const NicknameCheckScreen = () => {
         )}
       </View>
 
-      <TouchableOpacity style={styles.submitButton}>
-        <Text style={styles.submitButtonText}>완료</Text>
-      </TouchableOpacity>
+      {isIdChecked && !isDuplicate && (
+        <View>
+          {/* 비밀번호 입력 */}
+          <View style={styles.passwordRow}>
+            <TextInput
+              style={styles.input}
+              placeholder="비밀번호를 입력해주세요"
+              value={password}
+              onChangeText={(text) => {
+                validatePassword(text); // 비밀번호 유효성 검사
+                setPassword(text);
+              }}
+              secureTextEntry
+            />
+            {password && !isPasswordValid && (
+              <Text style={styles.passwordError}>
+                비밀번호는 영어(대문자, 소문자 구분), 숫자, 특수문자를 포함하여 8~16자 이어야 합니다.
+              </Text>
+            )}
+          </View>
+
+          {isPasswordValid && (
+            <View style={styles.passwordRow}>
+              {/* 비밀번호 확인 입력 */}
+              <TextInput
+                style={styles.input}
+                placeholder="비밀번호 확인"
+                value={passwordConfirm}
+                onChangeText={(text) => {
+                  handlePasswordConfirm(text)
+                }}
+                secureTextEntry
+              />
+             {passwordConfirm && !isPasswordConfirmed && (
+              <Text style={styles.passwordError}>비밀번호가 일치하지 않습니다.</Text>
+            )}
+            </View>
+          )}
+        </View>
+      )}
+
+      {isPasswordValid && !isDuplicate && isPasswordValid && isPasswordConfirmed &&(
+        <TouchableOpacity style={styles.submitButton}>
+          <Text style={styles.submitButtonText}>완료</Text>
+        </TouchableOpacity>
+      )}
 
     </View>
   );
@@ -88,18 +156,18 @@ const styles = StyleSheet.create({
   },
   input: {
     backgroundColor: '#F5F5F5',
-    borderRadius: 8,
+    borderRadius: 10,
     padding: 15,
     fontSize: 14,
-    marginBottom: 10,
+    marginBottom: 8,
   },
   button: {
     backgroundColor: '#04AD45',
-    borderRadius: 8,
+    borderRadius: 10,
     paddingVertical: 15,
     paddingHorizontal: 20,
     marginLeft: 10,
-    marginBottom: 10,
+    marginBottom: 8,
   },
   buttonText: {
     color: 'white',
@@ -119,9 +187,17 @@ const styles = StyleSheet.create({
     color: 'green',
     marginTop: 10,
   },
+  passwordRow: {
+    marginBottom: 10,
+  },
+  passwordError: {
+    color: '#04AD45',
+    fontSize: 12,
+    marginTop: 5,
+  },
   submitButton: {
     backgroundColor: '#04AD45',
-    borderRadius: 8,
+    borderRadius: 10,
     padding: 15,
     alignItems: 'center',
     marginTop: 30
